5ad5abc6a06789c9fc28a7f9a3f9f3e3c2b775f4
Guillermo Gonzalez <josegmgr@gmail.com>
2025-10-18 16:06:26 -0500

    refactor: limpieza de login y habilitado dashboard.

---
+++ core/views.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404
+from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import messages
+from django.http import JsonResponse
+import uuid
+
+# Create your views here.
+@login_required
+def home(request):
+    #Vista para la pagina principal
+
+    #Obtener datos para los widgets
+
+    #------------------------------
+    return render(request, 'core/dashboard.html')
--- membership_control/settings.py
+++ membership_control/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -52,15 +53,19 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'membership_control.urls'
 
+# AUTH SETTINGS
 AUTH_USER_MODEL = 'accounts.Usuario'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'login'
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
@@ -121,7 +126,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
