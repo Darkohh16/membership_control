b47876d89e358699f27b35ff5d370b44c626916a
Guillermo Gonzalez <josegmgr@gmail.com>
2025-10-20 17:50:28 -0500

    feat: views y templates iniciales de socios agregado.

--- socios/urls.py
+++ socios/urls.py
@@ -3,10 +3,12 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth.decorators import login_required
-from accounts.views import *
+from socios.views import *
 from core.views import *
 
 
 urlpatterns = [
-    path('socios/', include('accounts.urls')),
+    path('mostrar/', socios_view, name='socios_general'),
+    path('agregar/', nuevo_socio, name='nuevo_socio'),
+    path('<uuid:id>/editar/', editar_socio, name='editar_socio'),
 ]
