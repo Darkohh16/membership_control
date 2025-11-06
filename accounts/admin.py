6f01d7e3f51256a9ced3e33535f09ae0a13ce65a
Guillermo Gonzalez Rojas <152291078+Darkohh16@users.noreply.github.com>
2025-11-06 16:15:28 -0500

    Revert "Custodio"

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,25 +1,3 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import Usuario
 
-@admin.register(Usuario)
-class CustomUserAdmin(UserAdmin):
-    model = Usuario
-    list_display = ('username', 'first_name', 'last_name', 'email', 'perfil', 'is_active')
-    list_filter = ('perfil', 'is_active', 'is_staff')
-    search_fields = ('username', 'email', 'first_name', 'last_name')
-
-    fieldsets = (
-        (None, {'fields': ('username', 'password')}),
-        ('Información Personal', {'fields': ('first_name', 'last_name', 'email', 'perfil', 'avatar')}),
-        ('Permisos', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
-    )
-
-    add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('username', 'first_name', 'last_name', 'email', 'perfil', 'password1', 'password2'),
-        }),
-    )
-
-    ordering = ('username',)
+# Register your models here.
