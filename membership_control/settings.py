afe81ffd359c4edff4740558a791d0ea7ab18200
SantosInga20 <illontopsantosa@uss.edu.pe>
2025-11-08 01:21:47 -0500

    Agregar vista de detalle y asignación de membresía y actualizar plantillas

--- membership_control/settings.py
+++ membership_control/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'ms_control',
         'USER': 'postgres',
-        'PASSWORD': 'admin',
+        'PASSWORD': '6458',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
